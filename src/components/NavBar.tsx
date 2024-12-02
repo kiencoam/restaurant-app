360c9a606d1346b822c008d1cbdedf06a206aff4
Pham Trung Kien <kienpham1392004@gmail.com>
2024-12-02 12:48:18 +0700

    chờ gọi api lịch làm việc + chấm công

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -407,7 +407,7 @@ const NavBar = ({ role }) => {
               href="/home/order-taking"
             >
               <svg
-                className="w-7 h-7 ml-3"
+                className="w-5 h-5 ml-3"
                 aria-hidden="true"
                 xmlns="http://www.w3.org/2000/svg"
                 width="24"
